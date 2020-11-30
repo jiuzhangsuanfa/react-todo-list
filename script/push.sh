9e0ef40068731b656b35aff7d3401a4c02e40d6b
Devin Don <I.INF@Outlook.com>
2020-11-30 11:57:12 +0800

    perf(docker): remove useless docker file command

--- script/push.sh
+++ script/push.sh
@@ -4,8 +4,6 @@ pwd
 tee > ./Dockerfile <<-'EOF'
 FROM iinfinity/nginx
 COPY ["./", "/app/"]
-RUN ls /app
-RUN pwd
 EOF
 docker build -t registry.don.red/$npm_package_name .
 docker push registry.don.red/$npm_package_name
