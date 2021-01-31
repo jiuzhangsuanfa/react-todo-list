feca741788846cd64e4cccf79dd28ec582e0bea6
Devin Don <i.inf@outlook.com>
2021-01-31 11:16:02 +0800

    chore(script): update script

--- script/push.sh
+++ script/push.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-if [ ! -n "$registry" ];
-then
+if [ ! -n "$registry" ]; then
   registry='docker.io'
 fi
+
 cd build
-tee > ./Dockerfile <<-'EOF'
+tee >./Dockerfile <<-'EOF'
 FROM iinfinity/nginx
 COPY ["./", "/app/"]
 EOF
-docker build -t $registry/$npm_package_name .
-docker push $registry/$npm_package_name
+
+docker build -t $registry/$PUBLIC_URL .
+docker push $registry/$PUBLIC_URL
